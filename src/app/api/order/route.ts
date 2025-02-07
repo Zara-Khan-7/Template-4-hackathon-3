398cf493794f9e267fa84241158391ff1bccd6ba
Zara Yousuf Khan <zaara.khan1795.zy@gmail.com>
2025-02-08 01:04:55 +0500

    update

--- src/app/api/order/route.ts
+++ src/app/api/order/route.ts
@@ -3,7 +3,7 @@ import { createClient } from "@sanity/client";
 
 const client = createClient({
   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
-  dataset: process.env.SANITY_DATASET || "production",
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET ,
   useCdn: false,
   token: process.env.SANITY_API_TOKEN,
   apiVersion: "2023-01-01",
